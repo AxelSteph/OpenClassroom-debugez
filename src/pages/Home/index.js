2912e6f2542ba84af353942baafa493d5a7bbfd7
AxelSteph <axel.stephanus@gmail.com>
2026-07-17 15:54:02 +0200

    Problème de prop 'imageSrc' et 'title' réglé

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -115,14 +115,16 @@ const Page = () => {
     </main>
     <footer className="row">
       <div className="col presta">
-        <h3>Notre derniére prestation</h3>
-        <EventCard
-          imageSrc={last?.cover}
-          title={last?.title}
-          date={new Date(last?.date)}
-          small
+        <h3>Notre dernière prestation</h3>
+        {last && (
+          <EventCard
+            imageSrc={last.cover}
+            title={last.title}
+            date={new Date(last.date)}
+            small
           label="boom"
-        />
+          />
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
